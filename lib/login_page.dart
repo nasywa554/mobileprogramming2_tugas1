0bdf9825ff9896522fa1476e1ed9ee700490a32d
nasywa554 <nasywanurnafilah554@gmail.com>
2024-10-29 22:28:32 +0700

    menambahkan halaman login

---
+++ lib/login_page.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:mobileprogramming2_tugas1/home_page.dart';
+
+class LoginPage extends StatefulWidget {
+  static String tag = 'login-page';
+  @override
+  _LoginPageState createState() => new _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController usernameController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    // final email = TextFormField(
+    //   keyboardType: TextInputType.emailAddress,
+    //   autofocus: false,
+    //   initialValue: ' ',
+    //   decoration: InputDecoration(
+    //     hintText: 'Email',
+    //     contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+    //     border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+    //   ),
+    // );
+
+    final username = TextFormField(
+      keyboardType: TextInputType.text,
+      autofocus: false,
+
+      controller:
+          usernameController, // Associate the controller with the TextFormField
+      decoration: InputDecoration(
+        hintText: 'Username',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+    );
+
+    final password = TextFormField(
+      autofocus: false,
+      initialValue: ' ',
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: 'Password',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+    );
+
+    final loginButton = Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: Material(
+        borderRadius: BorderRadius.circular(30.0),
+        shadowColor: Colors.lightBlueAccent.shade100,
+        elevation: 5.0,
+        child: MaterialButton(
+          minWidth: 200.0,
+          height: 42.0,
+          onPressed: () {
+            String usernameValue = usernameController.text;
+            print(usernameValue);
+            Navigator.of(context)
+                .pushNamed(HomePage.tag, arguments: usernameValue);
+          },
+          color: Colors.lightBlueAccent,
+          child: Text('Log In', style: TextStyle(color: Colors.white)),
+        ),
+      ),
+    );
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: ListView(
+          shrinkWrap: true,
+          padding: EdgeInsets.only(left: 24.0, right: 24.0),
+          children: <Widget>[
+            SizedBox(height: 48.0),
+            username,
+            SizedBox(height: 8.0),
+            password,
+            SizedBox(height: 24.0),
+            loginButton,
+          ],
+        ),
+      ),
+    );
+  }
+}
